264d8f92d752e2df114ca7c0d443e0bd42c5946f
hardikanbati <hardikambati69@gmail.com>
2024-12-09 09:41:49 +0530

    patch : fixes

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY . /code/
 WORKDIR /code
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-CMD ["uvicorn", "--app-dir", "app/", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "--app-dir", "app/", "main:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/utils/query_enum.py
+++ app/utils/query_enum.py
@@ -4,6 +4,6 @@ from enum import Enum
 class QueryEnum(Enum):
 
     GET_ALL_TABLES = "SELECT name FROM sqlite_master where type='table'"
-    GET_TABLE_SCHEMA = "PRAGMA table_info({table})"
+    GET_TABLE_SCHEMA = "PRAGMA table_info('{table}')"
     GET_RECORDS = "SELECT * from {table}"
 
